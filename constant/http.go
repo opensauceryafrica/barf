84b1ae8150bad102b6c2efa1fa50b4f4d68d7411
Open Saucerer <lovedayperfection1@gmail.com>
2023-06-11 12:03:55 +0100

    chore: nifty methods and https support

---
+++ constant/http.go
@@ -0,0 +1,8 @@
+package constant
+
+const (
+	// Content-Type is the content type header
+	ContentType = "Content-Type"
+	// ApplicationJSON is the application/json content type
+	ApplicationJSON = "application/json"
+)
