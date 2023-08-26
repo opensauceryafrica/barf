4db7b1cb3dc6edaa00ed2a72cb9486a4727fc7dc
Open Saucerer <lovedayperfection1@gmail.com>
2023-08-26 17:05:44 +0100

    fix: host and documentation issue

--- env.go
+++ env.go
@@ -3,11 +3,11 @@ package barf
 import "github.com/opensaucerer/barf/env"
 
 /*
-Env loads environment variables into the given EnvStruct from the give EnvPath.
+Env loads environment variables into the given EnvStruct either from the os environment or from the give EnvPath.
 
-The EnvPath is optional and defaults to ".env".
+The EnvPath is optional. If no EnvPath is given, Env will attempt to load environment variables from the os environment.
 
-The struct must have a tag named "barfenv" with the following format:
+The struct provided must have a tag named "barfenv" with the following format:
 
 	`barfenv:"key=YOUR_ENV_KEY"`
 	`barfenv:"key=YOUR_ENV_KEY;required=true"`
--- env/env.go
+++ env/env.go
@@ -12,7 +12,14 @@ Env loads environment variables into the given EnvStruct either from the os envi
 
 The EnvPath is optional. If no EnvPath is given, Env will attempt to load environment variables from the os environment.
 
-The struct must have a tag named "barfenv" with the following format: "key=YOUR_ENV_KEY;required=true" or "key=YOUR_ENV_KEY;required=false".*/
+The struct provided must have a tag named "barfenv" with the following format:
+
+	`barfenv:"key=YOUR_ENV_KEY"`
+	`barfenv:"key=YOUR_ENV_KEY;required=true"`
+	`barfenv:"key=YOUR_ENV_KEY;required=false"`
+
+	`required` defaults to false if not specified.
+*/
 func Env(EnvStruct interface{}, EnvPath ...string) error {
 	if len(EnvPath) > 0 {
 		// load environment variables
